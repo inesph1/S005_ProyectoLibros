e633be8fb800b97f642d9f776f9904289b85b4b0
ines <145784317+inesph1@users.noreply.github.com>
2024-10-03 08:46:02 +0200

    Initial commit

---
+++ src/main/java/com/ipartek/S005ProyectoLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S005ProyectoLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S005ProyectoLibrosApplication.class, args);
+	}
+
+}
